8a0a8dc533904a693298f33b03ece20cd3002fe5
Sebastian Stamm <sebastian.stamm@camunda.com>
2017-10-28 15:42:49 +0200

    feat: voices

---
+++ config.js
@@ -0,0 +1,132 @@
+window.config = [
+  {
+  time: -30 * 60 * 1000,
+  speak: 'Welcome to the Zero Hour Game Jam. You have 30 Minutes left to prepare.',
+  voice: 'Google UK English Female',
+  pitch: 1
+  },
+  {
+  time: -10 * 60 * 1000,
+  speak: 'Welcome to the Zero Hour Game Jam. We start in 10 minutes. Get ready.',
+  voice: 'Google UK English Female',
+  pitch: 1
+  },
+  {
+  time: -1 * 60 * 1000,
+  speak: 'The Zero hour game jam starts in one minute.',
+  voice: 'Google UK English Female',
+  pitch: 1
+  },
+  {
+  time: -30 * 1000,
+  speak: 'T minus 30 seconds'
+  },
+  {
+  time: -10 * 1000,
+  speak: 'T minus 10 seconds'
+  },
+  {
+  time: -5 * 1000,
+  speak: '5'
+  },
+  {
+  time: -4 * 1000,
+  speak: '4'
+  },
+  {
+  time: -3 * 1000,
+  speak: '3'
+  },
+  {
+  time: -2 * 1000,
+  speak: '2'
+  },
+  {
+  time: -1 * 1000,
+  speak: '1'
+  },
+  {
+  time: 0,
+  speak: 'Begin'
+  },
+  {
+  time: 10 * 60 * 1000 - 1000,
+  speak: '50 minutes left'
+  },
+  {
+  time: 20 * 60 * 1000 - 1000,
+  speak: '40 minutes left'
+  },
+  {
+  time: 30 * 60 * 1000 - 1000,
+  speak: '30 minutes left'
+  },
+  {
+  time: 40 * 60 * 1000 - 1000,
+  speak: '20 minutes left'
+  },
+  {
+  time: 50 * 60 * 1000 - 1000,
+  speak: '10 minutes left'
+  },
+  {
+  time: 55 * 60 * 1000 - 1000,
+  speak: '5 minutes left'
+  },
+  {
+  time: 57 * 60 * 1000 - 1000,
+  speak: '3 minutes left'
+  },
+  {
+  time: 59 * 60 * 1000 - 1000,
+  speak: '60 seconds'
+  },
+  {
+  time: 59 * 60 * 1000 - 1000 + 30 * 1000,
+  speak: '30 seconds'
+  },
+  {
+  time: 59 * 60 * 1000 + 50 * 1000 - 1000,
+  speak: '10'
+  },
+  {
+  time: 59 * 60 * 1000 + 51 * 1000 - 1000,
+  speak: '9'
+  },
+  {
+  time: 59 * 60 * 1000 + 52 * 1000 - 1000,
+  speak: '8'
+  },
+  {
+  time: 59 * 60 * 1000 + 53 * 1000 - 1000,
+  speak: '7'
+  },
+  {
+  time: 59 * 60 * 1000 + 54 * 1000 - 1000,
+  speak: '6'
+  },
+  {
+  time: 59 * 60 * 1000 + 55 * 1000 - 1000,
+  speak: '5'
+  },
+  {
+  time: 59 * 60 * 1000 + 56 * 1000 - 1000,
+  speak: '4'
+  },
+  {
+  time: 59 * 60 * 1000 + 57 * 1000 - 1000,
+  speak: '3'
+  },
+  {
+  time: 59 * 60 * 1000 + 58 * 1000 - 1000,
+  speak: '2'
+  },
+  {
+  time: 59 * 60 * 1000 + 59 * 1000 - 1000,
+  speak: '1'
+  },
+  {
+  time: 60 * 60 * 1000,
+  speak: 'Game Over'
+  }
+]
